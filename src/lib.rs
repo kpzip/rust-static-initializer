10c4f2d3e69b698e9f1716af26dba2a97a0fd8bf
kpzip <kpzipgames@gmail.com>
2024-12-27 16:24:34 -0800

    test + docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,26 @@
-#![feature(raw_ref_op)]
+#![cfg_attr(not(test), no_std)]
+//! # Global non-lazy zero-cost statics without `const fn`.
+//! Useful for static values that cannot be initialized through `const fn` but cannot incur the memory & performance cost of a [`std::sync::LazyLock`].
+//!
+//! - See [`static_init!`]
+//!
+//! # `no_std` support
+//! this crate is `no_std`.
 
+#[doc(inline)]
 pub use static_initializer_macros::static_init;
-
-static_init! {
-    static TEST: i32 = static { 1 };
-}
 
 #[cfg(test)]
 mod tests {
-    //use super::*;
+    use super::*;
+
+    static_init! {
+        static TEST: Vec<u8> = unsafe static { (0..15u8).collect() };
+    }
 
     #[test]
     fn it_works() {
-        todo!()
+        // Should cause UB if something is weird
+        println!("Test vec: {:?}", TEST.as_slice());
     }
 }
